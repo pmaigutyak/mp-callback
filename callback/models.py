6854f7ae133ec0134922aab7456c25a0a2e9bb6e
Pasha Maigutyak <pmaigutyak@gmail.com>
2022-09-18 12:16:04 +0300

    Upgrade to django 4.

--- callback/models.py
+++ callback/models.py
@@ -1,5 +1,5 @@
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 from django.conf import settings
 from django.db import models
